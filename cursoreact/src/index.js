70ca1dc0e50f671d018a3bbc64bfc5477aab1a6f
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-01-31 00:16:32 -0300

    primeiro ola mundo em react 2

--- cursoreact/src/index.js
+++ cursoreact/src/index.js
@@ -1,4 +1,6 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
+import './index.css';
 import App from './App'
 
-ReactDOM.render(<App></App>, document.querySelector("#root"));
+
+ReactDOM.createRoot(document.querySelector("#root")).render(<App></App>);
